4d7c05249286ddce8d5133557647db36c3f10dca
chin0 <kafuuchin0>
2021-03-02 17:59:32 +0900

    asdf

--- chap2/set-binarytree.rkt
+++ chap2/set-binarytree.rkt
@@ -1,1 +1,99 @@
 #lang sicp
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (union-set s1 s2)
+  (if (or (equal? s1 '()) (equal? s2 '()))
+      '()
+      (let ((e1 (entry s1)) (e2 (entry s2)))
+        (cond ((= e1 e2) 
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+(define test1 (make-tree 7
+                         (make-tree 3
+                                    (make-tree 1 '() '())
+                                    (make-tree 5 '() '()))
+                         (make-tree 9
+                                    '()
+                                    (make-tree 11 '() '()))))
+(define test2 (make-tree 3
+                         (make-tree 1 '() '())
+                         (make-tree 7
+                                    (make-tree 5 '() '())
+                                    (make-tree 9 '()
+                                               (make-tree 11 '() '())))))
+
+(define test3 (make-tree 5
+                         (make-tree 3
+                                    (make-tree 1 '() '())
+                                    '())
+                         (make-tree 9
+                                    (make-tree 7 '() '())
+                                    (make-tree 11 '() '()))))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+                  (let ((right-tree (car right-result))
+                        (remaining-elts (cdr right-result)))
+                    (cons (make-tree this-entry left-tree right-tree)
+                          remaining-elts))))))))
+
+;2.65는 위에 두개 만든거 이용해서 orderedlist의 알고리즘 그대로 이용하면 됨.
+(list->tree '(1 3 5 7 9 11))
+
